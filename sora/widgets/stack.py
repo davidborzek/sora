9c67583bca27d380e9aab07ab1e7b73bf59c006c
David Borzek <mail@davidborzek.de>
2023-12-31 10:07:36 +0100

    add some more properties

--- sora/widgets/stack.py
+++ sora/widgets/stack.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import logging
 from sora.widgets.base import BaseWidget, BaseWidgetProps
 
 from gi.repository import Gtk, GObject
@@ -11,6 +12,8 @@ class StackProps(BaseWidgetProps):
     transition_type: Bindable[Gtk.StackTransitionType] = Gtk.StackTransitionType.NONE
     items: Bindable[dict[Gtk.Widget]] | None = None
     visible_child_name: Bindable[str] | None = None
+    vhomogeneous: Bindable[bool] = True
+    hhomogeneous: Bindable[bool] = True
 
 
 class Stack(BaseWidget(Gtk.Stack), Gtk.Stack):
@@ -21,6 +24,8 @@ class Stack(BaseWidget(Gtk.Stack), Gtk.Stack):
         self._bind_property("transition_type", props.transition_type)
         self._bind_property("items", props.items)
         self._bind_property("visible_child_name", props.visible_child_name)
+        self._bind_property("vhomogeneous", props.vhomogeneous)
+        self._bind_property("hhomogeneous", props.hhomogeneous)
 
     @GObject.Property(type=GObject.TYPE_PYOBJECT)
     def items(self):
@@ -32,6 +37,14 @@ class Stack(BaseWidget(Gtk.Stack), Gtk.Stack):
             current.destroy()
 
         for name, widget in items.items():
-            self.add_titled(widget, name, name)
+            self.add_named(widget, name)
 
         self.__items = items
+
+    def add_named(self, child: Gtk.Widget, name: str):
+        if name in self.__items:
+            logging.error(f"child with name {name} already in stack.")
+            return
+
+        self.__items[name] = child
+        super().add_named(child, name)
